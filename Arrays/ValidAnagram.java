248604102e0a496ef097edb5ca6b3daf15eb0c03
Swati Kumari Das <swatikumaridas1111@gmail.com>
2025-09-25 01:02:26 +0530

    Update ValidAnagram.java

--- Arrays/ValidAnagram.java
+++ Arrays/ValidAnagram.java
@@ -1,1 +1,43 @@
+//Brute Approach 
+//tc:O(n^2)
+//sc:O(n)
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] sSort = s.toCharArray();
+        char[] tSort = t.toCharArray();
+        Arrays.sort(sSort);
+        Arrays.sort(tSort);
+        return Arrays.equals(sSort, tSort);
+    }
+}
+
+//Hash Table (Using Array)
+//tc:O(n)
+//sc:O(1)
+class Solution {
+    public boolean isAnagram(String s, String t) {
+     if(s.length()!=t.length()){
+        return false;
+     }
+     int count[]=new int[26];
+
+     for(int i=0;i<s.length();i++){
+        char ch=s.charAt(i);
+        count[ch-'a']++;
+         count[t.charAt(i)-'a']--;
+     }
+   
+
+     for(int val:count){
+        if(val!=0){
+            return false;
+        }
+     }
+     return true;
+    }
+}
 
