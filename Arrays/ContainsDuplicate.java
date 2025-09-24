6f6f95fb248794c3ef1b6e11d3898667d4a99d40
Swati Kumari Das <swatikumaridas1111@gmail.com>
2025-09-25 00:54:57 +0530

    Add hasDuplicate method using HashMap
    Implemented a HashMap approach to check for duplicates in an array.

--- Arrays/ContainsDuplicate.java
+++ Arrays/ContainsDuplicate.java
@@ -15,3 +15,26 @@ public class Solution {
         return false;
     }
 }
+
+
+//HashMap Approach
+//tc:O(n)
+//sc:O(n)
+
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        
+      HashMap<Integer,Integer>map=new HashMap<>();
+
+      for(int num:nums){
+        map.put(num,map.getOrDefault(num,0)+1);
+      }
+
+      for(int val:map.values()){
+        if(val>1){
+            return true;
+        }
+      }
+      return false;
+    }
+}
