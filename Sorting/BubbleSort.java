f8fb5437902af6955138b6e3c042e0383f6efa9c
Swati Kumari Das <swatikumaridas1111@gmail.com>
2025-11-04 10:54:11 +0530

    Fix bubble sort loop to reduce iterations

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -19,7 +19,7 @@
 public class Solution {
     public void bubbleSort(int[] arr) {
         int n = arr.length;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n-1; i++) {
             boolean swap = false; // flag to check if any swap happened in this pass
 
             for (int j = 0; j < n - 1 - i; j++) {
